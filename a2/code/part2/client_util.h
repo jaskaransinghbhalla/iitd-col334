7a65197013a3fc0f597ce1695a78210bbe50629e
Jaskaran Singh Bhalla <jaskaransinghbhalla.mgsb@gmail.com>
2024-09-12 19:17:15 +0530

    multi clients file executed

--- a2/code/part2/client_util.h
+++ a2/code/part2/client_util.h
@@ -5,18 +5,24 @@
 #include <map>
 
 // Structure to hold client information
-struct ClientInfo {
+struct ClientInfo
+{
+    // Unique to client
     int client_id;
     int offset;
     std::map<std::string, int> wordFrequency;
+    // Shared among clients
+    int num_word_per_request;
+    int port;
+    int words_per_packet;
+    std::string ip_address;
 };
 
 // Function prototypes
-void read_config();
 int connect_to_server();
-void request_words(int client_sock_fd, ClientInfo* client_info);
-// void print_word_freq(const ClientInfo* client_info);
-void* client_thread(void* arg);
+void request_words(int client_sock_fd, ClientInfo *client_info);
+void print_word_freq(const ClientInfo *client_info);
+void *client_thread(void *arg);
 
 // Global variables (consider making these non-global in a real application)
 extern int num_clients;
