d3a860a7ae6ece7f906259fa30f4118a386205aa
Jaskaran Singh Bhalla <jaskaransinghbhalla.mgsb@gmail.com>
2024-10-03 16:22:38 +0530

    Spanning Tree added to part-3

--- a3/code/p3_spr.py
+++ a3/code/p3_spr.py
@@ -8,11 +8,13 @@ from ryu.lib.packet import ether_types
 from ryu.lib.packet import packet
 from ryu.ofproto import ofproto_v1_0
 from ryu.topology import event
+from ryu.lib.mac import haddr_to_bin
 from ryu.topology.api import get_switch, get_all_link
 import time
 import threading
 from ryu.lib.packet import packet
 from ryu.lib.packet import lldp
+import heapq
 
 
 class ShortestPathRouting(app_manager.RyuApp):
@@ -22,42 +24,109 @@ class ShortestPathRouting(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(ShortestPathRouting, self).__init__(*args, **kwargs)
-        self.graph = {}
-        self.w_graph = {}
-        self.topology_api_app = self
+        
+        # LLDP
+        
         self.lldp_active = True  # Flag to control LLDP handling
         self.lldp_duration = 10  # Duration to handle LLDP packets in seconds
         self.src_lldp_timestamps = {}
+        
+        # Common
+        
+        self.mac_to_port = {}
+        self.graph = {}
+        self.links = []
+        self.switches = []
+        self.topology_api_app = self
+        
+        # Shortest Path
+        
+        self.w_graph = {}
+        self.shortest_path = {}
+
+        # Spanning Tree
+        
+        self.blocked_ports = set()
+        self.spanning_tree = []
+        
         threading.Thread(target=self.lldp_timer).start()
+        
 
     # LLDP Packet and Graph construction
 
     @set_ev_cls(event.EventSwitchEnter)
     def get_topology_data(self, event):
-
-        # Send LLDP Packets
-
-        self.switches = get_switch(self.topology_api_app, None)
-        for switch in self.switches:
-            if self.lldp_active:
-                self.send_lldp_packets_on_all_ports(switch.dp)
-
-        print("--------------------------------")
-        print("Graph")
-
+        
+        # Switches and Links
+        
         self.switches = get_switch(self.topology_api_app, None)
         self.links = [link.to_dict() for link in get_all_link(self.topology_api_app)]
-        self.graph = {}
-        graph = self.graph
+        print(get_all_link(self.topology_api_app))
+        # time.sleep(5000000)
+        
+        # Graph Construction
+        
+        graph = {}
         for link in self.links:
             src = link["src"]["dpid"]
             dst = link["dst"]["dpid"]
             if int(dst) not in graph.get(int(src), []):
-                self.graph.setdefault(int(src), []).append(int(dst))
+                graph.setdefault(int(src), []).append(int(dst))
             if int(src) not in graph.get(int(dst), []):
                 graph.setdefault(int(dst), []).append(int(src))
-        print(graph)
-        print("--------------------------------")
+        self.graph = graph
+        
+        # Send LLDP Packets
+        
+        for switch in self.switches:
+            if self.lldp_active:
+                self.send_lldp_packets_on_all_ports(switch.dp)
+        self.create_spanning_tree()
+        
+
+        
+    def create_spanning_tree(self):
+        
+        # Spanning Tree
+        self.prims()
+        self.block_ports(self.spanning_tree)
+        
+    
+    def prims(self):
+        visited = set()
+        start = self.switches[0].dp.id
+        
+        visited.add(start)
+        edges = []
+        for neighbor in self.graph[start]:
+            if neighbor not in visited:
+                edges.append((start, neighbor))
+
+        while edges:
+            edges.sort(key=lambda x: x[1])  # You can use different criteria for sorting
+            next_edge = edges.pop(0)
+            src, dst = next_edge
+
+            if dst not in visited:
+                visited.add(dst)
+                self.spanning_tree.append(next_edge)
+
+                # Add new edges from the newly visited node
+                for neighbor in self.graph[dst]:
+                    if neighbor not in visited:
+                        edges.append((dst, neighbor))
+
+    def block_ports(self, spanning_tree):
+        self.blocked_ports.clear()  # Reset blocked ports
+        for link in self.links:
+            src = link["src"]["dpid"]
+            dst = link["dst"]["dpid"]
+            if (int(src), int(dst)) not in spanning_tree and (
+                int(dst),
+                int(src),
+            ) not in spanning_tree:
+                self.blocked_ports.add((int(src), int(link["src"]["port_no"])))
+                self.blocked_ports.add((int(dst), int(link["dst"]["port_no"])))
 
     def send_lldp_packets_on_all_ports(self, datapath):
         """
@@ -71,12 +140,13 @@ class ShortestPathRouting(app_manager.RyuApp):
 
                 # Build LLDP packet for each port
                 lldp_pkt = self.build_lldp_packet(datapath, port_no)
+                self.src_lldp_timestamps[(datapath.id, port_no)] = time.time()
                 self.send_packet(
                     datapath, port_no, lldp_pkt
                 )  # Create an OpenFlow PacketOut message to send the LLDP packet
                 
                 # Save the time.time() in src_lldp_timestamps
-                self.src_lldp_timestamps[(datapath.id, port_no)] = time.time()
+                
                 print(f"LLDP Packet Sent {datapath.id} {port_no}", time.time())
 
     def build_lldp_packet(self, datapath, port_no):
@@ -96,10 +166,7 @@ class ShortestPathRouting(app_manager.RyuApp):
             ),  # Convert DPID to bytes
         )
 
-        # port_id = lldp.PortID(
-        #     subtype=lldp.PortID.SUB_PORT_COMPONENT,
-        #     port_id=str(port_no).encode(),  # Convert port number to bytes
-        # )
+       
         port_id = lldp.PortID(
             subtype=lldp.PortID.SUB_PORT_COMPONENT,
             port_id=struct.pack(
@@ -145,13 +212,58 @@ class ShortestPathRouting(app_manager.RyuApp):
 
     def lldp_timer(self):
         time.sleep(self.lldp_duration)  # Wait for the duration
+        self.lldp_active = False  # Stop handling LLDP packets
+        self.cal_shortest_path(self.w_graph)
         print("LLDP Timer expired.")
+        print("--------------------------------")
+        print("Graph")
+        print(self.graph)
         print("--------------------------------")
         print("Modified Graph")
         print(self.w_graph)
-        self.lldp_active = False  # Stop handling LLDP packets
+        print("--------------------------------")
+        print("Spanning-Tree")
+        print(self.spanning_tree)
+        print("--------------------------------")
+        print("Blocked Ports")
+        for dpid, port_no in self.blocked_ports:
+            print(f"DPID: {dpid}, Port: {port_no}")
+        print("--------------------------------")
+        print("Shortest Path")
+        print(self.shortest_path)
+        print("--------------------------------")
 
     # Packet In Handling
+    
+    def cal_shortest_path(self, graph):
+        for node in graph:
+            self.shortest_path[node] = self.dijkstra(graph, node)
+    
+    def dijkstra(self, graph, start):
+        # Initialize the distance dictionary with infinity
+        distances = {node: float('inf') for node in graph}
+        distances[start] = 0  # Distance to the start node is 0
+
+        # Priority queue: (distance to the node, node)
+        pq = [(0, start)]
+
+        while pq:
+            current_distance, current_node = heapq.heappop(pq)
+
+            # If the distance is greater than the recorded shortest path, skip it
+            if current_distance > distances[current_node]:
+                continue
+
+            # Explore neighbors
+            for neighbor, weight in graph[current_node].items():
+                distance = current_distance + weight
+
+                # If a shorter path to the neighbor is found
+                if distance < distances[neighbor]:
+                    distances[neighbor] = distance
+                    heapq.heappush(pq, (distance, neighbor))
+
+        return distances
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, event):
@@ -159,6 +271,7 @@ class ShortestPathRouting(app_manager.RyuApp):
         datapath = msg.datapath
         ofp = datapath.ofproto
         ofp_parser = datapath.ofproto_parser
+        
         pkt = packet.Packet(msg.data)  # Parsing the packet to get the Ethernet frame
         eth = pkt.get_protocol(
             ethernet.ethernet
@@ -167,7 +280,60 @@ class ShortestPathRouting(app_manager.RyuApp):
             if self.lldp_active:
                 self.handle_lldp_packet_in(datapath, msg, pkt)
             return
+        
+       
+        dst = eth.dst
+        src = eth.src
+        dpid = datapath.id
 
+        self.mac_to_port.setdefault(dpid, {})
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = msg.in_port
+        out_ports = []
+        actions = []
+        flooding = False
+        
+        # Port Management
+        
+        
+        if dst in self.mac_to_port[dpid]:
+            out_ports = [self.mac_to_port[dpid][dst]]
+        else:
+            all_ports = set(datapath.ports.keys())
+            excluded_ports = {msg.in_port, 65534}
+            for id, port_no in self.blocked_ports:
+                if id == dpid:
+                    excluded_ports.add(port_no)
+            out_ports = all_ports - excluded_ports
+            flooding = True
+            
+    # Get all available ports
+        
+    # Exclude the ports
+        # out_ports = all_ports - excluded_ports
+        # If there are valid ports, forward the packet; otherwise, drop it
+        if out_ports:
+            actions = [ofp_parser.OFPActionOutput(port) for port in out_ports]
+        else:
+            actions = []  # Drop packet
+        # install a flow to avoid packet_in next time
+        if not flooding:
+            self.add_flow(datapath, msg.in_port, dst, src, actions)
+            # print("Updated Flow Table")
+
+        data = None
+        if msg.buffer_id == ofp.OFP_NO_BUFFER:
+            data = msg.data
+        out = ofp_parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=msg.in_port,
+            actions=actions,
+            data=data,
+        )
+        datapath.send_msg(out)
+        
     def handle_lldp_packet_in(self, datapath, msg, pkt):
         # Parse the LLDP packet
         lldp_pkt = pkt.get_protocol(lldp.lldp)
@@ -196,7 +362,7 @@ class ShortestPathRouting(app_manager.RyuApp):
                 if time.time() - start_time > self.lldp_duration:
                     return
             end_time = time.time()
-            link_delay = round((end_time - start_time) * 1000, 2)  # Convert to milliseconds and round to 2 decimal places
+            link_delay = round((end_time - start_time) * 1000, 0)  # Convert to milliseconds and round to 2 decimal places
 
             #    Store the graph in w_graph
             self.w_graph.setdefault(src_chassis, {})[datapath.id] = link_delay
@@ -219,3 +385,29 @@ class ShortestPathRouting(app_manager.RyuApp):
 
             #   Continue processing, such as updating network topology or logging neighbor connection
             # self.update_topology(src_dpid, src_port, neighbor_dpid, src_port, neighbor_port)
+
+     # Add flow to the switch
+    
+    def add_flow(self, datapath, in_port, dst, src, actions):
+        ofp = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+
+        match = datapath.ofproto_parser.OFPMatch(
+            in_port=in_port, dl_dst=haddr_to_bin(dst), dl_src=haddr_to_bin(src)
+        )
+
+        # Modification
+        # The following code is modified to add the flow with the OFPFF_SEND_FLOW_REM flag set.
+        mod = ofp_parser.OFPFlowMod(
+            datapath=datapath,
+            match=match,
+            cookie=0,
+            command=ofp.OFPFC_ADD,
+            idle_timeout=0,
+            hard_timeout=0,
+            priority=ofp.OFP_DEFAULT_PRIORITY,
+            flags=ofp.OFPFF_SEND_FLOW_REM,
+            actions=actions,
+        )
+        datapath.send_msg(mod)
+
